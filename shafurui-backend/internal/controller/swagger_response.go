774fd1fd0079b0585c46bb99e2074017f2c64588
kenevoco <kenevoco@gmail.com>
2026-07-02 22:29:50 +0800

    refactor: 移除 MyCode 类型，统一使用整数类型作为响应代码

--- shafurui-backend/internal/controller/swagger_response.go
+++ shafurui-backend/internal/controller/swagger_response.go
@@ -3,19 +3,19 @@ package controller
 import "shafurui/internal/model"
 
 type SwaggerErrorResponse struct {
-	Code    MyCode      `json:"code" example:"1001"`
+	Code    int         `json:"code" example:"1001"`
 	Message string      `json:"message" example:"请求参数错误"`
 	Data    interface{} `json:"data"`
 }
 
 type SwaggerHelloInfoResponse struct {
-	Code    MyCode                     `json:"code" example:"1000"`
+	Code    int                        `json:"code" example:"1000"`
 	Message string                     `json:"message" example:"success"`
 	Data    model.GetHelloInfoResponse `json:"data"`
 }
 
 type SwaggerAuthLoginResponse struct {
-	Code    MyCode                  `json:"code" example:"1000"`
+	Code    int                     `json:"code" example:"1000"`
 	Message string                  `json:"message" example:"success"`
 	Data    model.AuthLoginResponse `json:"data"`
 }
