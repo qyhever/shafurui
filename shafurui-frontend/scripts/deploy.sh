3df21ab7957a4aa6c541a2d7bdc54b8a002582bb
kenevoco <kenevoco@gmail.com>
2026-07-05 22:58:03 +0800

    feat: 更新 README 文档，新增 Nginx 配置和部署脚本

---
+++ shafurui-frontend/scripts/deploy.sh
@@ -0,0 +1,2 @@
+npm run build
+rsync -avz --delete dist/ kr:/var/www/html/sfr/
--- shafurui-frontend/src/router/index.ts
+++ shafurui-frontend/src/router/index.ts
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import { useUserStore } from '@/stores/user'
-import HomeView from '../views/HomeView.vue'
 import LoginView from '../views/LoginView.vue'
 
 const HOME_PATH = '/home'
@@ -16,7 +15,7 @@ const router = createRouter({
     {
       path: '/home',
       name: 'home',
-      component: HomeView,
+      component: () => import('../views/HomeView.vue'),
     },
     {
       path: LOGIN_PATH,
